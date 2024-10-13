9ad447a35f8e2e1e38990da968a4b1efafe71655
Chen Hao Ming <2825493439@qq.com>
2024-10-13 09:03:13 +0000

    2024 10 13 17:03

--- Unit_one/low_read.c
+++ Unit_one/low_read.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<fcntl.h>
 #include<unistd.h>
+#include<fcntl.h>
+#define BUF_SIZE 100
 void error_handling(char *message) {
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
 }
 int main(void) {
-    int fd;
-    char buf[] = "Let's go!\n";
-    fd = open("data.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    int fd = open("data.txt", O_RDONLY);
     if(fd == -1) {
         error_handling("open() error");
     }
     printf("file descriptor: %d \n", fd);
-
-    if(write(fd,buf,sizeof(buf)) == -1) {
-        error_handling("write() error");
+    char BUF[BUF_SIZE];
+    if(read(fd,BUF,sizeof(BUF)) == -1) {
+        error_handling("read() error");
     }
+    printf("file data: %s", BUF);
     close(fd);
     return 0;
 }
